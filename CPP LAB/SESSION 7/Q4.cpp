da28f8f9d92cebf91033f169ea4598547c51788b
BENIN GEORGE VARGHESE <48290297+Benin45@users.noreply.github.com>
2019-11-14 09:53:50 +0530

    Added some files into the S7 folder
    5,6,7 shared

--- CPP LAB/SESSION 7/Q4.cpp
+++ CPP LAB/SESSION 7/Q4.cpp
@@ -1,37 +1,44 @@
-/*
-Using main method, input an integer below 60. Using a class called Fib generate the
-Fibonacci series till this integer. Use constructor.
-*/
-#include <iostream.h>
-#include <conio.h>
-#include<dos.h>
-
-class Fib{
-int Number;
-public:
-
-Fib(int N){
-Number=N;
-
-}
-
-// ==> Doesn't matter what you are working on
-
-
+/*Using main method, input an integer below 60.
+Using a class called Fib generate the Fibonacci series till this integer. Use constructor*/
+#include<iostream.h>
+#include<conio.h>
+class Fib
+{
+	public:
+	double i,t,x,y,p,z;
+	Fib(int v)
+	{
+		t=v;
+		x=0;
+		y=1;
+	}
+	void ser()
+	{
+	cout<<"The fibonacci series:"<<x<<","<<y<<",";
+	p=2;
+	while(p<t)
+	   {
+		z=x+y;
+		p++;
+		cout<<z<<",";
+		x=y;
+		y=z;
+	   }
+	}
 };
-
-int main(){
-int Number;
+void main()
+{
 	clrscr();
-
-	cout<<"SUM OF N NUMBER CALCULATOR"<<endl;
-	cout<<"Enter any natural number : ";
-	cin>>Number;
-	 Fib trial(Number);
-	
-	 
-	trial.math_process();
-
+	int n;
+	cout<<"Input an integer below 60:";
+	cin>>n;
+	if(n<60)
+	{
+		Fib h(n);
+		h.ser();
+	}
+	else{
+		cout<<"Shrug......";
+	}
 	getch();
-	return 0;
 }
