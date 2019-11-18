9f108a14681602c895788d7c6c4410b3abefa9e3
BENIN GEORGE VARGHESE <48290297+Benin45@users.noreply.github.com>
2019-11-18 13:36:21 +0530

    S9 1 to 9

---
+++ CPP LAB/SESSION 9/Q4.cpp
@@ -0,0 +1,16 @@
+/*Create a class ‘Student’, and inherit another class ‘Undergrad’ from this class.
+The parent class Student shall have the following protected data members: name, ID, grade,
+age, and address and a public method: boolean Passed (double grade)
+The Passed () method willtake one parameter- grade (value between 0 and 100). If the grade
+is above 70.0, then Passed() returns true, otherwise it returns false.
+Input the grade using a public method accept () in the child class.*/
+#include<iostream.h>
+#include<conio.h>
+class Student{
+
+
+};
+class Undergrad: public Student
+{
+
+};
